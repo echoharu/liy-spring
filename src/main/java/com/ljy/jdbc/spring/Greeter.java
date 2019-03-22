f3535d7107b53ee74f4dfb8d2465d6d53a2aaca9
ljy <dlwnddus9389@naver.com>
2019-03-22 14:57:08 +0900

    추가

---
+++ src/main/java/com/ljy/jdbc/spring/Greeter.java
@@ -0,0 +1,12 @@
+package com.ljy.jdbc.spring;
+
+public class Greeter {
+
+	public Greeter() {
+		System.out.println("Greeter instance created.");
+		}
+		
+		public String greet() {
+			return "Hello, World!";
+		}
+}
