260b86299ffba74d2dd6bcf9c66237946187b202
ljy <dlwnddus9389@naver.com>
2019-04-05 14:37:52 +0900

    ...

---
+++ src/main/java/org/ljy/article/ArticleService.java
@@ -0,0 +1,73 @@
+package org.ljy.article;
+
+import java.util.List;
+
+/**
+ * articleDao를 사용해서 글 목록, 조회, 추가, 수정, 삭제를 한다.
+ * 
+ * @author Jacob
+ */
+public class ArticleService {
+
+	ArticleDao articleDao;
+
+	public void setArticleDao(ArticleDao articleDao) {
+		this.articleDao = articleDao;
+	}
+
+	/**
+	 * 글 목록
+	 */
+	public void listArticles() {
+		List<Article> articles = articleDao.listArticles();
+		System.out.println(articles);
+	}
+
+	/**
+	 * 글 조회
+	 */
+	public void getArticle() {
+		Article article = articleDao.getArticle("3");
+		System.out.println(article);
+	}
+
+	/**
+	 * 글 등록
+	 */
+	public void addArticle() {
+		Article article = new Article();
+		article.setTitle("강원도 산불, 2000년 동해 산불 재현되나? 여의도 100배 면적 전소");
+		article.setContent(
+				"고성에서 발생해 강원도 속초, 삼척 등지로 확산된 강원도 산불을 진화하는 데 헬기 50여대와 800명대 이상의 소방차가 투입됐다. 다만 건조한 날씨와 강풍으로 불길을 완전히 잡지는 못한 상태다. 이 때문에 현재까지....");
+		article.setUserId("1");
+		article.setName("이중연");
+		if (articleDao.addArticle(article) > 0)
+			System.out.println("글을 추가했습니다.");
+		else
+			System.out.println("글을 추가하지 못했습니다.");
+	}
+
+	/**
+	 * 글 수정
+	 */
+	public void updateArticle() {
+		Article article = new Article();
+		article.setArticleId("7");
+		article.setTitle("This is modified title.");
+		article.setContent("This is modified content");
+		if (articleDao.updateArticle(article) > 0)
+			System.out.println("글을 수정했습니다.");
+		else
+			System.out.println("글을 수정하지 못했습니다.");
+	}
+
+	/**
+	 * 글 삭제
+	 */
+	public void deleteArticle() {
+		if (articleDao.deleteArticle("8") > 0)
+			System.out.println("글을 삭제했습니다.");
+		else
+			System.out.println("글을 삭제하지 못했습니다.");
+	}
+}
